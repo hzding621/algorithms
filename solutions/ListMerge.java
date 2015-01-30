5391b796bd3616b0fd79a4c4e9c21bc3c6244d1b
Haozhen Ding <hzding621@gmail.com>
2015-01-29 20:39:54 -0600

    add

---
+++ solutions/ListMerge.java
@@ -0,0 +1,53 @@
+// https://oj.leetcode.com/problems/merge-two-sorted-lists/
+
+public class ListMerge {
+
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		if (l2 == null)
+			return l1;
+		else if (l1 == null)
+			return l2;
+		
+		ListNode head = null;
+		ListNode ret = null;
+		if (l1.val <= l2.val) {
+			ret = head = l1;
+			l1 = l1.next;
+		}
+		else {
+			ret = head = l2;
+			l2 = l2.next;
+		}
+		
+		while (l1 != null && l2 != null ) {
+			if (l1.val <= l2.val) {
+				head.next = l1;
+				head = head.next;
+				l1 = l1.next;
+			} else {
+				head.next = l2;
+				head = head.next;
+				l2 = l2.next;
+			}
+		}
+		
+		if (l1 != null) {
+			while (l1 != null) {
+				head.next = l1;
+				head = head.next;
+				l1 = l1.next;
+			}
+		}
+		else {
+			while (l2 != null) {
+				head.next = l2;
+				head = head.next;
+				l2 = l2.next;
+			}
+		}
+		
+		return ret;
+		
+	}
+	
+}
