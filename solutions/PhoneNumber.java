b05958e61c9251ec00e3f622f72cefeffdc8f81b
Haozhen Ding <hzding621@gmail.com>
2015-01-29 21:51:33 -0600

    addstart medium

--- solutions/PhoneNumber.java
+++ solutions/PhoneNumber.java
@@ -2,135 +2,72 @@ import java.util.*;
 
 public class PhoneNumber {
 
-	/*.
-	Print all valid phone numbers of length n subject to following constraints:
-	1.If a number contains a 4, it should start with 4
-	2.All digits are distinct
-	3.Three digits (e.g. 7,2,9) will be entirely disallowed, take as input
-	*/
 
-	public static boolean checkDistinct(List<Integer> list) {
-		if (list.size() > 10)
-			return false;
-		boolean[] array = new boolean[10];
-		for (int i : list) {
-			if (array[i] == true)
-				return false;
-			array[i] = true;
-		}
-		return true;
-	}
+    static HashMap<Integer, ArrayList<Character>> table = null;
+    
+    public static void preprocess() {
+        if (table == null) {
+            table = new HashMap<Integer, ArrayList<Character>>();
+            ArrayList<Character> two = new ArrayList<Character>();
+            two.add('a'); two.add('b'); two.add('c');
+            table.put(2, two);
+            ArrayList<Character> three = new ArrayList<Character>();
+            three.add('d'); three.add('e'); three.add('f');
+            table.put(3, three);
+            ArrayList<Character> four = new ArrayList<Character>();
+            four.add('g'); four.add('h'); four.add('i');
+            table.put(4, four);
+            ArrayList<Character> five = new ArrayList<Character>();
+            five.add('j'); five.add('k'); five.add('l');
+            table.put(5, five);
+            ArrayList<Character> six = new ArrayList<Character>();
+            six.add('m'); six.add('n'); six.add('o');
+            table.put(6, six);
+            ArrayList<Character> seven = new ArrayList<Character>();
+            seven.add('p'); seven.add('q'); seven.add('r'); seven.add('s');
+            table.put(7, seven);
+            ArrayList<Character> eight = new ArrayList<Character>();
+            eight.add('t'); eight.add('u'); eight.add('v'); 
+            table.put(8, eight);
+            ArrayList<Character> nine = new ArrayList<Character>();
+            nine.add('w'); nine.add('x'); nine.add('y'); nine.add('z'); 
+            table.put(9, nine);
+            table.put(1, new ArrayList<Character>());
+            table.put(0, new ArrayList<Character>());
+        }
+    }
+    public static List<String> letterCombinations(String digits) {
+    	preprocess();
+        List<String> result = new ArrayList<String>();
+        result.add("");
 
-	public static List<List<Integer>> compute(List<Integer> disallowed, int n) {
-		List<List<Integer>> perm = permute(n);
-		return teleCheck(perm, disallowed);
-	}
+        for (int i=0; i<digits.length(); i++) {
+        	int digit = digits.charAt(i) - '0';
+        	// System.out.println(digit);
+        	List<Character> allChars = table.get(digit);
+        	for (char ch: allChars) {
+        		// System.out.println(ch + " " + digit);
+        	}
 
-	public static List<List<Integer>> teleCheck(List<List<Integer>> permute, List<Integer> disallowed){
+        	List<String> newStrings = new ArrayList<String>();
+        	for (Character c: allChars) {
+        		for (String str: result) {
+        			String newStr = str + c;
+        			newStrings.add(newStr);
+        		}
+        	}
+        	result = newStrings;
+        }
+        return result;
+    }
 
-		List<List<Integer>> ret = new ArrayList<List<Integer>>();
-
-		for (List<Integer> l : permute) {
-			boolean add = true;
-
-			if (checkDistinct(l) == false) {
-				add = false;
-				continue;
-			}
-
-			for (int i=0; i<l.size(); i++) {
-				int num = l.get(i);
-
-				if (num == 4 && i != 0) {
-					add = false;
-					break;
-				}
-
-				for (int j=0; j<disallowed.size(); j++) {
-					if (disallowed.get(j) == num) {
-						add = false;
-						break;
-					}
-				}
-
-				if (add == false)
-					break;
-			}
-
-			if (add == true)
-				ret.add(l);
-		}
-
-		return ret;
-
-
-	}
-
-
-	public static List<List<Integer>> permute(int n) {
-		/* generate all lists of length n */
-
-		List<List<Integer>> ret = new ArrayList<List<Integer>>();
-
-		if (n == 0)
-			return ret;
+    public static void main(String[] args) {
+    	List<String> lists = letterCombinations("2222");
+    	for (String s: lists) {
+    		System.out.println(s);
+    	}
+    }
 
 
-		for (int i=0; i<10; i++) {
-			List<Integer> newItem = new ArrayList<Integer>();
-			newItem.add(i);
-			ret.add(newItem);
-		}
-		n--;
-		while (n > 0) {
-
-			List<List<Integer>> cache = new ArrayList<List<Integer>>();
-
-			for (List<Integer> l: ret) {
-				for (int i=1; i<10; i++) {
-					List<Integer> newL =  new ArrayList<Integer>(l);
-					newL.add(i);
-					cache.add(newL);
-				}
-				l.add(0);
-			}
-
-			for (List<Integer> m: cache)
-				ret.add(m);
-			n--;
-		}
-
-		return ret;
-
-	}
-
-	public static void printList(List<Integer> list) {
-		
-		System.out.print("[");
-		for (int i=0; i<list.size() - 1; i++) {
-			System.out.print(list.get(i) + ",");
-		}
-		System.out.println(list.get(list.size()-1) + "]"); 
-	}
-
-	public static void printDoubleList(List<List<Integer>> lists) {
-		for (List<Integer> l: lists) {
-			printList(l);
-		}
-	}
-
-	public static void main(String[] args) {
-		List<Integer> disallowed = new ArrayList<Integer>();
-
-		disallowed.add(0);
-		disallowed.add(1);
-		disallowed.add(2);
-		disallowed.add(3);
-		disallowed.add(5);
-		disallowed.add(6);
-
-		List<List<Integer>> ret = compute(disallowed, 3);
-		printDoubleList(ret);
-	}
 
 }
