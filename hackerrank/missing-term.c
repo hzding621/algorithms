22f23c80bea9e0bc190496c473fbaa628fe35de4
Haozhen Ding <hzding621@gmail.com>
2015-02-09 20:31:53 -0600

    add

--- hackerrank/missing-term.c
+++ hackerrank/missing-term.c
@@ -2,44 +2,44 @@
 #include <stdlib.h>
 
 int find(int* array, int n) {
-    if (n == 3) {
-        return (array[1] - array[0] + array[1]);
-    } else {
-        int diff2 = array[2] - array[1];
-        int diff1 = array[1] - array[0];
-        if (diff1 != diff2) {
-            int diff3 = array[3] - array[2];
-            if (diff3 == diff2)
-                return array[1] - diff3;
-            else 
-                return (2 * array[1] - array[0]);
-        } else {
-            
-            int j = 3;
-            for (; j<n ; j++) {
-                
-                int diffx = array[j] - array[j-1];
-                if (diffx != diff1) {
-                    return array[j-1] + diff1;
-                }
-                
-            }
+	if (n == 3) {
+		return (array[1] - array[0] + array[1]);
+	} else {
+		int diff2 = array[2] - array[1];
+		int diff1 = array[1] - array[0];
+		if (diff1 != diff2) {
+			int diff3 = array[3] - array[2];
+			if (diff3 == diff2)
+				return array[1] - diff3;
+			else 
+				return (2 * array[1] - array[0]);
+		} else {
+			
+			int j = 3;
+			for (; j<n ; j++) {
+				
+				int diffx = array[j] - array[j-1];
+				if (diffx != diff1) {
+					return array[j-1] + diff1;
+				}
+				
+			}
 
-        }
-        
-    }
-    return -1;
+		}
+		
+	}
+	return -1;
 }
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    int n;
-    scanf("%d\n", &n);
-    int *array = malloc(sizeof(int) * n);
-    int j=0;
-    for(; j<n; j++) {
-        scanf("%d ", array+j);
-    }
-    printf("%d\n", find(array, n));
-    return 0;
+	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
+	int n;
+	scanf("%d\n", &n);
+	int *array = malloc(sizeof(int) * n);
+	int j=0;
+	for(; j<n; j++) {
+		scanf("%d ", array+j);
+	}
+	printf("%d\n", find(array, n));
+	return 0;
 }
--- hackerrank/tree-diameter.c
+++ hackerrank/tree-diameter.c
@@ -6,8 +6,8 @@
 #include <limits.h>
 #define max( a, b ) ( ((a) > (b)) ? (a) : (b) )
 struct node {
-   struct node *left,*right;
-   int val, val1;
+	 struct node *left,*right;
+	 int val, val1;
 };
 typedef struct node node;
 node * addElement(node * root, int x);
@@ -15,90 +15,90 @@ node * addRandomElement(node *, int, int);
 
 /* Write your custom functions here */
 struct result {
-    int height;
-    int diameter;
+		int height;
+		int diameter;
 };
 typedef struct result result;
 
 
 result recurse(node *root){
-    result res;
-    if (root == NULL) {
-        res.height = 0;
-        res.diameter = 0;
-    } else {
-        result res1 = recurse(root->left);
-        result res2 = recurse(root->right);
-        res.height = 1+max(res1.height, res2.height);
-        res.diameter = max(max(res1.diameter, res2.diameter), 1+res1.height+res2.height);
-    }
-    return res;
+		result res;
+		if (root == NULL) {
+				res.height = 0;
+				res.diameter = 0;
+		} else {
+				result res1 = recurse(root->left);
+				result res2 = recurse(root->right);
+				res.height = 1+max(res1.height, res2.height);
+				res.diameter = max(max(res1.diameter, res2.diameter), 1+res1.height+res2.height);
+		}
+		return res;
 }
-    
+		
 int diameterOfTree(node * root) {
 /* For your reference
-  struct node {
-   struct node *left,*right;
-   int val;
+	struct node {
+	 struct node *left,*right;
+	 int val;
 };
 */
-    result res = recurse(root);
-    return res.diameter;
+		result res = recurse(root);
+		return res.diameter;
 }
 
 int main(){
-   
-   FILE *f = fopen(getenv("OUTPUT_PATH"), "w");
-   node * root = NULL;
-   int a[1000],K,i = 0,j = 0;
-   int isBst = 0; scanf("%d",&isBst);    
-   
-   scanf("%d",&K);
-   for( j = 0; j < K;j++ ) {
-       scanf("%d",&a[i++]);    
-   }
 
-   for( i = 0; i < K;i++ ){
-       if( !isBst ){            
-           root = addRandomElement(root,a[i],i);
-       } else {
-           root = addElement(root,a[i]);
-       }            
-   }
-   fprintf(f, "%d\n", diameterOfTree(root));
-   fclose(f);
-   
+	FILE *f = fopen(getenv("OUTPUT_PATH"), "w");
+	node * root = NULL;
+	int a[1000],K,i = 0,j = 0;
+	int isBst = 0; scanf("%d",&isBst);    
+
+	scanf("%d",&K);
+	for( j = 0; j < K;j++ ) {
+		 scanf("%d",&a[i++]);    
+	}
+
+	for( i = 0; i < K;i++ ){
+		 if( !isBst ){            
+				 root = addRandomElement(root,a[i],i);
+		 } else {
+				 root = addElement(root,a[i]);
+		 }            
+	}
+	fprintf(f, "%d\n", diameterOfTree(root));
+	fclose(f);
+	 
 return 0;
 }
 node * addElement(node * root, int x ){
-   if( root == NULL ) {
-       root = (node *) (malloc(sizeof(node))); 
-       root->val = x;root->val1 = -1;
-               root->left = NULL; root->right = NULL;
-       return root;
-   }
-   if( x < root->val ) {
-        root->left = addElement(root->left,x);
-   }
-   else {
-       root->right = addElement(root->right,x);                            
-   }
-   return root;
+	if( root == NULL ) {
+		 root = (node *) (malloc(sizeof(node))); 
+		 root->val = x;root->val1 = -1;
+						 root->left = NULL; root->right = NULL;
+		 return root;
+	}
+	if( x < root->val ) {
+			root->left = addElement(root->left,x);
+	}
+	else {
+		 root->right = addElement(root->right,x);                            
+	}
+	return root;
 }
 
 node * addRandomElement(node *root, int x, int index){
-   if( root == NULL ) {
-       root = (node *) (malloc(sizeof(node))); 
-       root->val = x;root->val1 = -1;
-               root->left = NULL; root->right = NULL;
-               return root;
-   }
+	if( root == NULL ) {
+		 root = (node *) (malloc(sizeof(node))); 
+		 root->val = x;root->val1 = -1;
+						 root->left = NULL; root->right = NULL;
+						 return root;
+	}
 
-   if( index <=2 ){
-       root->left = addRandomElement(root->left,x,index);
-   } else {
-       root->right = addRandomElement(root->right,x,index);
-   }
+	if( index <=2 ){
+		 root->left = addRandomElement(root->left,x,index);
+	} else {
+		 root->right = addRandomElement(root->right,x,index);
+	}
 
-   return root;
+	return root;
 }
