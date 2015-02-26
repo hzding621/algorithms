598dcd0d2f103e3ba65e23b996ddc4dab1edb6c5
Haozhen Ding <hzding621@gmail.com>
2015-02-25 21:09:26 -0600

    update

--- Helpers.java
+++ Helpers.java
@@ -12,6 +12,30 @@ public class Helpers {
 		System.out.println();
 	}
 
+	public static void print(boolean[] array) {
+		System.out.println(Arrays.toString(array));
+		// for (int i=0; i<array.length; i++)
+		// 	System.out.print(array[i] ? 1 : 0);
+		// System.out.println();
+	}
+
+	public static void print(boolean[][] matrix) {
+		for (int i=0; i<matrix.length; i++) {
+			print(matrix[i]);
+		}
+		System.out.println();
+	}
+
+	public static void print(char[] array) {
+		System.out.println(Arrays.toString(array));
+	}
+	public static void print(char[][] matrix) {
+		for (int i=0; i<matrix.length; i++) {
+			print(matrix[i]);
+		}
+		System.out.println();
+	}
+
 	public static void print(int[] array, int length) {
 		if (length > array.length)
 			print(array);
