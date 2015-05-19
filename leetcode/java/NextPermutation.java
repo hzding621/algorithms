3ccc27c1d8ff95848a5a08b29aba0b5c43b3b2e6
Haozhen Ding <hzding621@gmail.com>
2015-05-18 21:44:00 -0500

    0518

--- leetcode/java/NextPermutation.java
+++ leetcode/java/NextPermutation.java
@@ -6,22 +6,10 @@ public class NextPermutation {
 		nums[j] = t;
 	}
 
-	static void sort(int[] nums, int i, int j) {
-		// quick sort
-		if (i >= j)
-			return;
-		int mid = nums[(i+j)/2];
-		swap(nums, (i+j)/2, j);
-		int t = i;
-		for (int k=i; k<=j-1; k++) {
-			if (nums[k] <= mid) {
-				swap(nums, t, k);
-				t++;
-			}
+	static void reverse(int[] nums, int i, int j) {
+		for (int k=i; k<=(i+j)/2; k++) {
+			swap(nums, k, i+j-k);
 		}
-		swap(nums, t, j);
-		sort(nums, i, t-1);
-		sort(nums, t+1, j);
 	}
 
 	public static void nextPermutation(int[] nums) {
@@ -38,21 +26,20 @@ public class NextPermutation {
 			int ini = nums[i-1];
 			int next = i;
 			for (int k=i; k<=nums.length-1; k++) {
-				if (nums[k] > ini && nums[k] < nums[next])
+				if (nums[k] > ini && nums[k] <= nums[next]) // must swap to the right-most position to preserve non-increasing order
 					next = k;
 			}
 			swap(nums, i-1, next);
-			sort(nums, i, nums.length-1);
+			reverse(nums, i, nums.length-1);
 		} else {
-			swap(nums, 0, nums.length-1);
-			sort(nums, 1, nums.length-1);
+			reverse(nums, 0, nums.length-1);
 		}
 		return;
 	}
 
 	public static void main(String[] args) {
-		int[] num = {1,2,3,4,5};
-		int times = 121;
+		int[] num = {1,2,3,4};
+		int times = 25;
 		while (times > 0) {
 			times--;
 			Helpers.print(num);
