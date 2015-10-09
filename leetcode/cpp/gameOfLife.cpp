743e28ed99dab9f0f20ea183b6b56d2f0c5dcf10
Haozhen Ding <haozhending@wirelessprvnat-172-17-108-124.near.illinois.edu>
2015-10-08 19:50:30 -0500

    10/08

--- leetcode/cpp/gameOfLife.cpp
+++ leetcode/cpp/gameOfLife.cpp
@@ -3,26 +3,16 @@
 #include "codingUtility.cpp"
 using namespace std;
 void update(vector<vector<int>>& board, int i, int j) {
-  // cout << "(" << i << "," << j << ")" << endl;
-  int liveNeighbors = 0;
-  if (i > 0 && board[i-1][j] & 1)
-    liveNeighbors++;
-  if (i < board.size()-1 && board[i+1][j] & 1)
-    liveNeighbors++;
-  if (j > 0 && board[i][j-1] & 1)
-    liveNeighbors++;
-  if (j < board[0].size() && board[i][j+1] & 1)
-    liveNeighbors++;
-  if (i > 0 && j > 0 && board[i-1][j-1] & 1)
-    liveNeighbors++;
-  if (i > 0 && j < board[0].size()-1 && board[i-1][j+1] & 1)
-    liveNeighbors++;
-  if (i < board.size()-1 && j > 0 && board[i+1][j-1] & 1)
-    liveNeighbors++;
-  if (i < board.size()-1 && j < board[0].size()-1 && board[i+1][j+1] & 1)
-    liveNeighbors++;
-  if ((board[i][j] && (liveNeighbors == 2 || liveNeighbors == 3))
-      || (!board[i][j] && liveNeighbors == 3) )
+  int m = board.size(), n = board[0].size();
+  int count = 0;
+  for (int ii=max(0, i-1); ii<=min(m-1, i+1); ii++) {
+    for (int jj=max(0, j-1); jj<=min(n-1, j+1); jj++) {
+      if (ii == i && jj == j)
+        continue;
+      if (board[ii][jj] & 1) count ++;
+    }
+  }
+  if (count == 3 || count - board[i][j] == 2)
     board[i][j] |= 2;
 }
 void gameOfLife(vector<vector<int>>& board) {
@@ -33,18 +23,11 @@ void gameOfLife(vector<vector<int>>& board) {
   }
   for (int i=0; i<board.size(); i++) {
     for (int j=0; j<board[0].size(); j++) {
-      board[i][j] = board[i][j] >> 1;
+      board[i][j] >>= 1;
     }
   }
 }
 int main() {
-  vector<vector<int>> v = generate2DIntArray("[[0,0,0,0,0,0],[0,0,1,1,0,0],[0,1,0,0,1,0],[0,0,1,0,1,0],[0,0,0,1,0,0],[0,0,0,0,0,0]]");
-  gameOfLife(v);
-  print2DIntArray(v);
-  cout << endl;
-  std::vector<std::vector<int>> sol = generate2DIntArray(
-    "[[0,0,0,0,0,0],[0,0,1,1,0,0],[0,1,0,0,1,0],[0,0,1,0,1,0],[0,0,0,1,0,0],[0,0,0,0,0,0]]"
-  );
-  print2DIntArray(sol);
+  
   return 0;
 }
