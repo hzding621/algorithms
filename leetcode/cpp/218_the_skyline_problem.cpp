e10fe49bd1c506c3eda94d3d9f47a458edd2107f
Haozhen Ding <hzding621@gmail.com>
2016-08-04 00:21:21 -0700

    cpp/218

---
+++ leetcode/cpp/218_the_skyline_problem.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) {
+        set<pair<int,pair<int, int>>> uniqueVert;
+        for (vector<int>& triple: buildings) {
+        	int l = triple[0], r = triple[1], h = triple[2];
+        	auto left_mark = make_pair(l, make_pair(-h, -1));
+        	auto right_mark = make_pair(r, make_pair(h, l));
+        	uniqueVert.insert(left_mark);
+        	uniqueVert.insert(right_mark);
+        }
+        vector<pair<int,pair<int, int>>> sortedVert;
+        sortedVert.assign(uniqueVert.begin(), uniqueVert.end());
+        sort(sortedVert.begin(), sortedVert.end());
+
+        // for (auto& trip: sortedVert) {
+        // 	cout << trip.first << " " << trip.second.first << " " << trip.second.second << endl;
+        // }
+
+        map<int, int> activeSet; // height -> count
+        vector<pair<int,int>> skylinePoints;
+        for (int i=0; i<sortedVert.size(); i++) {
+        	auto& mark = sortedVert[i];
+        	int pos = mark.first, previousPos = mark.second.second;
+        	if (previousPos < 0) {
+        		int h = -mark.second.first;
+        		if (activeSet.empty() || activeSet.rbegin()->first < h) {
+        			skylinePoints.push_back(make_pair(pos, h));
+        		}
+        		activeSet[h]++;
+        	} else {
+        		int h = mark.second.first;
+        		activeSet[h]--;
+
+        		if (activeSet[h] == 0) {
+        			activeSet.erase(h);
+        		}
+        		if (activeSet.empty()) {
+        			skylinePoints.push_back(make_pair(pos,0));
+        		} else {
+        			if (activeSet.rbegin()->first < h) {
+        				skylinePoints.push_back(make_pair(pos, activeSet.rbegin()->first));
+        			}
+        		}
+        	}
+        }
+        return skylinePoints;
+    }
+};
+
+int main() {
+	Solution sol;
+	vector<vector<int>> b;
+
+	/*{
+		//[2 9 10], [3 7 15], [5 12 12], [15 20 10], [19 24 8] 
+		b.push_back(vector<int>{2,9,10});
+		b.push_back(vector<int>{3,7,15});
+		b.push_back(vector<int>{5,12,12});
+		b.push_back(vector<int>{15,20,10});
+		b.push_back(vector<int>{19,24,8});
+	}*/
+
+	/*{
+		//[[0,2,3],[2,5,3]]
+		b.push_back(vector<int>{0,2,3});
+		b.push_back(vector<int>{2,5,3});
+	}*/
+
+	/*{
+		// [[1,2,1],[1,2,2],[1,2,3]]
+		b.push_back(vector<int>{1,2,1});
+		b.push_back(vector<int>{1,2,2});
+		b.push_back(vector<int>{1,2,3});
+	}*/
+
+	/*{
+		// [[1,3,4],[1,3,4],[1,3,4]]
+		b.push_back(vector<int>{1,3,4});
+		b.push_back(vector<int>{1,3,4});
+		b.push_back(vector<int>{1,3,4});
+	}*/
+
+	/*{ // [[4,10,10],[5,10,9],[6,10,8],[7,10,7],[8,10,6],[9,10,5]]
+		b.push_back(vector<int>{4,10,10});
+		b.push_back(vector<int>{5,10,9});
+		b.push_back(vector<int>{6,10,8});
+		b.push_back(vector<int>{7,10,7});
+		b.push_back(vector<int>{8,10,6});
+		b.push_back(vector<int>{9,10,5});
+	}*/
+	{
+		b.push_back(vector<int>{1,2,2});
+		b.push_back(vector<int>{2,3,1});
+	}
+
+	auto res = sol.getSkyline(b);
+	for (auto& p: res) {
+		cout << p.first << " " << p.second << endl;
+	}
+
+}
