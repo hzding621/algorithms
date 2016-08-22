71271c85d483054d3c261b10d46450d6317c00ae
Haozhen Ding <hzding621@gmail.com>
2016-08-22 13:13:45 -0500

    Refactored version

--- leetcode/cpp/341_flatten_nested_list_iterator.cpp
+++ leetcode/cpp/341_flatten_nested_list_iterator.cpp
@@ -35,21 +35,23 @@ private:
 
 class NestedIterator {
 public:
-    NestedIterator(vector<NestedInteger> &nestedList) {
-        _input = nestedList;
-        pullFromInput();
+    NestedIterator(vector<NestedInteger> &nestedList) 
+    : _input(nestedList) { 
+        populateStackIfNecessary();
     }
 
     int next() {
-        if (_nextInputIsSingleton) {
-            int result = _input[_indexIntoInput++].getInteger();
-            pullFromInput();
+        int result;
+        if (currentInputIsSingleton()) {
+            result = _input[_indexIntoInput++].getInteger();
+            populateStackIfNecessary();
             return result;
-        }
-        int result = _stackNi.top().getList()[_stackIndex.top()].getInteger();
-        incrementStackTop();
-        seek();
-        return result;
+        } else {
+            result = getCurrentNestInt().getInteger();
+            incrementStackTop();
+            seekSingleton();
+            return result;
+        }   
     }
 
     bool hasNext() {
@@ -57,78 +59,80 @@ public:
     }
 private:
 
-    vector<NestedInteger> _input;
-    int _indexIntoInput = 0;
-    bool _nextInputIsSingleton = false;
-
     bool _hasNext = true;
-    stack<NestedInteger> _stackNi;
-    stack<int> _stackIndex;
+    const vector<NestedInteger>& _input;
+    int _indexIntoInput = 0;
+    stack<NestedInteger> _stackOfInts;
+    stack<int> _stackOfActiveIndices;
 
-    void seek() {
-        int hasPulledFromInput = false;
+    bool currentInputIsSingleton() {
+        return _input[_indexIntoInput].isInteger();
+    }
+
+    void seekSingleton() {
         while (true) {
-            while (_stackIndex.top() == _stackNi.top().getList().size()) {
-                _stackIndex.pop();
-                _stackNi.pop();
-                if (_stackNi.empty()) {
-                    pullFromInput();
-                    hasPulledFromInput = true;
-                    break;
+            while (_stackOfActiveIndices.top() == _stackOfInts.top().getList().size()) {
+                _stackOfActiveIndices.pop();
+                _stackOfInts.pop();
+                if (_stackOfInts.empty()) {
+                    // advance to next input
+                    _indexIntoInput++;
+                    populateStackIfNecessary();
+                    return;
                 }
                 incrementStackTop();
             }
-            if (!hasPulledFromInput && !(_stackNi.top().getList()[_stackIndex.top()].isInteger())) {
-                _stackNi.push(_stackNi.top().getList()[_stackIndex.top()]);
-                _stackIndex.push(0);
-            } else {
-                break;
-            }
+            if (getCurrentNestInt().isInteger())
+                return;
+
+            _stackOfInts.push(getCurrentNestInt());
+            _stackOfActiveIndices.push(0);
         }
        
     }
 
     void incrementStackTop() {
-        int i = _stackIndex.top();
-        _stackIndex.pop();
-        _stackIndex.push(i+1);
+        int i = _stackOfActiveIndices.top();
+        _stackOfActiveIndices.pop();
+        _stackOfActiveIndices.push(i+1);
     }
 
-    void pullFromInput() {
+    void populateStackIfNecessary() {
 
         if (_indexIntoInput == _input.size()) {
             _hasNext = false;
             return;
         }
 
-        if (_input[_indexIntoInput].isInteger()) {
-            _nextInputIsSingleton = true;
-        } else {
-            _stackNi.push(_input[_indexIntoInput++]);
-            _stackIndex.push(0);
-            _nextInputIsSingleton = false;
-            seek();
+        if (!currentInputIsSingleton()) {
+            _stackOfInts.push(_input[_indexIntoInput]);
+            _stackOfActiveIndices.push(0);
+            seekSingleton();
         }
+    }
+
+    NestedInteger getCurrentNestInt() {
+        return _stackOfInts.top().getList()[_stackOfActiveIndices.top()];
     }
 };
 
 int main() {
 
     vector<NestedInteger> v = {
-        NestedInteger(1),
+        // NestedInteger(1),
         NestedInteger(vector<NestedInteger>{
-            // NestedInteger(2),
+            NestedInteger(2),
             NestedInteger(
                 vector<NestedInteger>{
-                    // NestedInteger(3),
-                    // NestedInteger(4)
+                    NestedInteger(3),
+                    NestedInteger(4)
             })
         }),
         NestedInteger(5),
         NestedInteger(
             vector<NestedInteger>{
-                // NestedInteger(6),
-                // NestedInteger(7)
+                NestedInteger(6),
+                NestedInteger(7)
         }),
         NestedInteger(8)
     };
